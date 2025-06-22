e7c01f8775c1967da0356b30128deea3d474b1eb
Robert Luby <luby.robo@gmail.com>
2025-06-22 20:21:32 +0200

    landing page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { useAuth } from "../providers/AuthProvider";
-import AdminPage from "./admin/page";
 import LoginPage from "./login/page";
 
 export default function LandingPage() {
-  const { user, isLoading } = useAuth();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  return user ? <AdminPage /> : <LoginPage />;
+  return <LoginPage />;
 }
